ff9da9608cde14a71a14de8c2f3df4820b0f384d
ʚ 一休さん ɞ <phannawatlaptop09@gmail.com>
2024-09-22 15:09:26 +0700

    Update

---
+++ proj/atnow/app/(tabs)/screens/shorts.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ShortsScreen = () => {
+  return (
+    <View>
+      <Text>Shorts Section</Text>
+      {/* Implement shorts */}
+    </View>
+  );
+};
+
+export default ShortsScreen;
