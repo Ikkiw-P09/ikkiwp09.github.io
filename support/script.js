6cab411d009e547b409eabb0bc5a1fcae14abc50
ʚ 一休さん ɞ <phannawatlaptop09@gmail.com>
2024-12-09 20:35:50 +0700

    Support & Proj Update

--- support/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("myBtn").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent default link behavior
-    document.getElementById("promptpaypopup").style.display = "block";
-  });
-
-  document.getElementById("closeBtn").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent default link behavior
-    document.getElementById("promptpaypopup").style.display = "none";
-  });
